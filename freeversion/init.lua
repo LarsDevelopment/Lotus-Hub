b22b6ba88fb166e02eaf5f54ebb5255bcb90d2de
Lars <info.larsvfx@gmail.com>
2024-11-17 20:12:18 +0100

    Update init.lua

--- freeversion/init.lua
+++ freeversion/init.lua
@@ -1,1 +1,13 @@
+local SupportedList ={
+    "4639625707", -- Wartycoon
+}
 
+for i,v in pairs(SupportedList) do
+    if tostring(v) == tostring(game.PlaceId) then
+        loadstring(game:HttpGet("https://raw.githubusercontent.com/LarsDevelopment/Lotus-Hub/refs/heads/main/freeversion/games/".. tostring(v) ..".lua"))()
+    else
+        game:GetService("Players").LocalPlayer:Kick("Unsupported Game")
+    end
+end
+
+    
